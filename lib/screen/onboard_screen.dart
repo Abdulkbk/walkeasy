583070678462a2eeec47e2014e3db2bf2c231336
Abdulkbk <abdoollahikbk@gmail.com>
2022-12-21 17:40:55 +0100

    onboarding screen base

--- lib/screen/onboard_screen.dart
+++ lib/screen/onboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class OnboardScreen extends StatefulWidget {
   const OnboardScreen({Key? key}) : super(key: key);
@@ -25,8 +26,67 @@ class _OnboardScreenState extends State<OnboardScreen> {
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: const [
-              Text('wow')
+            children: [
+              SizedBox(height: h * 0.04,),
+              Container(
+                // color: Colors.black.withOpacity(0.2),
+                width: w,
+                height: h * 0.20,
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Best Shoes, \nfor the modern walkers',
+                      style: TextStyle(
+                        fontSize: 36.0,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.grey[900],
+                      ),
+                    ),
+                    const SizedBox(height: 20.0,),
+                    Text(
+                      'Polish up your moves to make yourself more beautiful with this app',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.grey[900],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10.0,),
+              Container(
+                color: Colors.black.withOpacity(0.2),
+                width: w,
+                height: h * 0.5,
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: const Image(
+                  image: AssetImage('assets/img/hero_img.png'),
+                ),
+              ),
+              const SizedBox(height: 70.0,),
+              Container(
+                // color: Colors.black.withOpacity(0.2),
+                width: w,
+                height: 60.0,
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: ElevatedButton(
+                  onPressed: () => Get.toNamed('/login'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)
+                    )
+                  ),
+                  child: const Text(
+                    'Get started',
+                    style: TextStyle(
+                      fontSize: 22.0
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
